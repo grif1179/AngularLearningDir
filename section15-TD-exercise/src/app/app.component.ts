da744006419ff8ef009267c7242923d53dab5114
Grant Park <grif1179@gmail.com>
2019-10-18 15:27:07 -0400

    Finished Section 15 TD project

--- section15-TD-exercise/src/app/app.component.ts
+++ section15-TD-exercise/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("f", {static: false}) formObj: NgForm;
 
+  onSubmit() {
+    console.log(this.formObj.value.email);
+    console.log(this.formObj.value.sub);
+    console.log(this.formObj.value.password);
+  }
 }
