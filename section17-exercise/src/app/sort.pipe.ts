431c29564bfcc54f39cf548413fca7de25df0f97
Grant Park <GPark@humana.com>
2019-10-21 14:53:11 -0400

    Added Pipe exercise

---
+++ section17-exercise/src/app/sort.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any): any {
+    return value.slice().sort(
+      (a, b) => {
+        if(a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+
+        return 0;
+      }
+    );
+  }
+}
